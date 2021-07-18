2fe124612e463e808b2a14220b3bfe665e7d0a35
hema <hemabkrishnan98@gmail.com>
2021-07-18 19:31:16 +0530

    calculator

---
+++ calc.js
@@ -0,0 +1,14 @@
+function displayBox(num){
+    inpt.value+=num;
+}
+function evaluateExpression (){
+    let val=inpt.value
+    let result=eval(val)
+    inpt.value=result
+}
+function clearBox(){
+    inpt.value=""
+}
+function backspace(){
+    inpt.value=inpt.value.slice(0,-1)
+}
